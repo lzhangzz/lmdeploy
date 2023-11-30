1ebb7678957f2c2780be0175663896192144a951
Li Zhang <lzhang329@gmail.com>
2023-11-30 06:43:31 +0000

    add missing

--- src/turbomind/kernels/gemm_s_f16/common.h
+++ src/turbomind/kernels/gemm_s_f16/common.h
@@ -186,27 +186,44 @@ __inline__ __device__ void ldmatrix_m8n8_x2_b16(uint& d0, uint& d1, uint32_t sme
 #endif
 }
 
-__inline__ __device__ void wait_flag(int* lock, int status, int thread_id)
+__inline__ __device__ int sem_fetch(int* lock, bool pred)
+{
+    int state{};
+    if (pred) {
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 700
+        asm volatile("ld.global.acquire.gpu.b32 %0, [%1];\n" : "=r"(state) : "l"(lock));
+#else
+        asm volatile("ld.global.cg.b32 %0, [%1];\n" : "=r"(state) : "l"(lock));
+#endif
+    }
+    return state;
+}
+
+__inline__ __device__ void sem_wait(int* lock, int status, bool pred)
 {
     int state = 0;
     while (__syncthreads_and(state != status)) {
-        if (thread_id == 0) {
-#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 700
-            asm volatile("ld.global.acquire.gpu.b32 %0, [%1];\n" : "=r"(state) : "l"(lock));
-#else
-            asm volatile("ld.global.cg.b32 %0, [%1];\n" : "=r"(state) : "l"(lock));
-#endif
-        }
+        state = sem_fetch(lock, pred);
     }
 
     __syncthreads();  // memory fence
 }
 
-__inline__ __device__ void release_flag(int* lock, int status, int thread_id)
+__inline__ __device__ void sem_wait_many(int* lock, int count, bool pred)
+{
+    int state = 0;
+    while (__syncthreads_count(state) != count) {
+        state = sem_fetch(lock, pred);
+    }
+
+    __syncthreads();  // memory fence
+}
+
+__inline__ __device__ void sem_post(int* lock, int status, bool pred)
 {
     __syncthreads();  // memory fence
 
-    if (thread_id == 0) {
+    if (pred) {
 #if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 700
         asm volatile("st.global.release.gpu.b32 [%0], %1;\n" : : "l"(lock), "r"(status));
 #else
